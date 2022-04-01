c8eea28e6c2c5952ee049a0282a5fe0aecc69e59
cortex225 <48181439+cortex225@users.noreply.github.com>
2022-04-01 14:04:05 -0400

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TP4_Ajax.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
