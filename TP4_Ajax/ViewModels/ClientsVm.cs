13108ade1e8e99cdff7aebf4e39a3f33b691e2d1
cortex225 <48181439+cortex225@users.noreply.github.com>
2022-04-04 16:02:22 -0400

    AddVM

---
+++ TP4_Ajax/ViewModels/ClientsVm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using TP4_Ajax.Models;
+
+namespace TP4_Ajax.ViewModels
+{
+    public class ClientsVm
+    {
+        public int ClientId { get; set; }
+
+        [DisplayName("Nom")]
+        public string Nom { get; set; }
+
+        public int Age { get; set; }
+
+        public string Courriel { get; set; }
+
+        public string NoTelephone { get; set; }
+
+        public int AbonnementId { get; set; }
+        public Abonnement Abonnement { get; set; }
+    }
+}
