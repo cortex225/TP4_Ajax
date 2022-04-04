8b3a2aa20bb6ad7efb207ad268b29905f357827f
cortex225 <48181439+cortex225@users.noreply.github.com>
2022-04-04 14:56:34 -0400

    add

---
+++ Seeder/DbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using TP4_Ajax.Data;
+
+namespace TP3.Seeder
+{
+    public class DbContextFactory  // : IDesignTimeDbContextFactory<Exercice3_ModeleEFCoreDBContext>
+    {
+        public static TPDbContext CreateDbContext()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(@Directory.GetCurrentDirectory() + "/appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<TPDbContext>();
+            var connectionString = configuration.GetConnectionString("MyConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new TPDbContext(builder.Options);
+        }
+    }
+}
